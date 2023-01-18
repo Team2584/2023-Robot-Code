a4fbdf7f3d9ba17dccfd8d3e5b0a447cb3d32002
Driver Station <75519663+drago314@users.noreply.github.com>
2023-01-17 17:10:22 -0800

    Add Debugging Info

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -261,6 +261,7 @@ void Robot::TeleopPeriodic()
 
   for (auto array : poseSub.ReadQueue()) 
   {
+    frc::SmartDashboard::PutNumber("NT X", array.value[0]);
     Pose2d poseEst = Pose2d(units::meter_t{array.value[0]}, units::meter_t{array.value[1]}, Rotation2d(units::radian_t{array.value[3]}));
     SmartDashboard::PutNumber("Network Table Last Update Time", array.time);
     swerveDrive->AddPositionEstimate(poseEst, units::second_t{array.time + array.value[4]});
