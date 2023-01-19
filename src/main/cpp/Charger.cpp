302cb9fc1cf7c3823773459ad2645fa6dfbf4456
DRKDesigns <75279720+DRKDesigns@users.noreply.github.com>
2023-01-18 20:24:50 -0800

    idk what I am doing at this point

--- src/main/cpp/Charger.cpp
+++ src/main/cpp/Charger.cpp
@@ -1,24 +1,31 @@
 void Swerve::driveToCharger(Pose2d current){
     if (onCharger)
     {
-        if (balenced)
+        if (current.Rotation().X() == 0)
         {
-            strafe();
+            if (current.Rotation().Z() != 90)
+            {
+                pidTurnWheelsTo(90)
+            }
         }
         else
         {
-            pidDriveTo(current.Rotation());
+            pidDriveTo(current.Rotation().X());
         }
     }
     else
     {
         if (current.Y() > goalYpos)
         {
-            DriveToPose(current, spotAboveGoal, double elapsedTime, double translationMaxSpeed, double translationMaxAccel, double allowableErrorTranslation, double translationP, double translationI, double translationIMaxEffect, double rotationMaxSpeed, double rotationMaxAccel, double allowableErrorRotation, double rotationP, double rotationI, double rotationIMaxEffect, double useWeirdMinSpeedThing);
+            DriveToPose(current, 
+            //TODO spotAboveGoal, 
+            double elapsedTime, double translationMaxSpeed, double translationMaxAccel, double allowableErrorTranslation, double translationP, double translationI, double translationIMaxEffect, double rotationMaxSpeed, double rotationMaxAccel, double allowableErrorRotation, double rotationP, double rotationI, double rotationIMaxEffect, double useWeirdMinSpeedThing);
         }
         else
         {
-            DriveToPose(current, spotBelowGoal, double elapsedTime, double translationMaxSpeed, double translationMaxAccel, double allowableErrorTranslation, double translationP, double translationI, double translationIMaxEffect, double rotationMaxSpeed, double rotationMaxAccel, double allowableErrorRotation, double rotationP, double rotationI, double rotationIMaxEffect, double useWeirdMinSpeedThing);
+            DriveToPose(current, 
+            //TODO spotBelowGoal, 
+            double elapsedTime, double translationMaxSpeed, double translationMaxAccel, double allowableErrorTranslation, double translationP, double translationI, double translationIMaxEffect, double rotationMaxSpeed, double rotationMaxAccel, double allowableErrorRotation, double rotationP, double rotationI, double rotationIMaxEffect, double useWeirdMinSpeedThing);
         }
     }
 }
