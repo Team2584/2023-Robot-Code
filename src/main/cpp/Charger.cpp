6286e1239f4b60449810781b89b55b4989b3ce7a
DRKDesigns <75279720+DRKDesigns@users.noreply.github.com>
2023-01-28 10:53:34 -0800

    Added past code

--- src/main/cpp/Charger.cpp
+++ src/main/cpp/Charger.cpp
@@ -31,5 +31,16 @@ void Swerve::driveToCharger(Pose2d current){
 }
 
 void Swerve::balence(Pose2d current){
-    DriveSwervePercent(0, 0.4, 0);
+      gyroRot = gyroScope.GetPitch();     //Pull pitch angle from gyroscope
+    frc::SmartDashboard::PutNumber("gyroRot", gyroRot); //on the dashboard, output the gyroRot number
+    float deadZone = 2.5;                           //deadzone angle
+    float motorMaxSpeed = 0.02;                     //max speed of motor in %
+    float motorVelocity;                            //final velocity of motor
+    int direction = (gyroRot > 0) ? -1 : 1;         // if gyroRot is greater than 0, change direction to -1, vice versa. This is for correction, we want to move opposite direction from tilt
+
+    if (abs(gyroRot) > deadZone)
+        motorVelocity = direction * (abs(gyroRot) - deadZone) * motorMaxSpeed * 1 / 9; //when rotation of gyro exceeds the deadzone, set motor velocity (this is proportional to the gyro angle)
+    if (motorVelocity > 0.4)
+        motorVelocity = 0.4;
+    DriveSwervePercent(0, motorVelocity, 0);
 }
